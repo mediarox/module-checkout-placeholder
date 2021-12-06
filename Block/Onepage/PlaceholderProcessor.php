092ff85fbe3cda79e9b4a9df5fb41e677731b0d1
Steven Fritzsche <sfritzsche@mediarox.de>
2021-12-06 12:17:06 +0100

    [*] layoutProcessor > refactoring [*] layoutProcessor, di.xml > split field search into list & specific field [*] helper > refactoring [*] README.md > refactoring [+] README.md > wiki link > How do I add a custom checkout field ? [-] README.md > todo's (should be created as issues)

--- Block/Onepage/PlaceholderProcessor.php
+++ Block/Onepage/PlaceholderProcessor.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright 2021 (c) mediarox UG (haftungsbeschraenkt) (http://www.mediarox.de)
+ * See LICENSE for license details.
+ */
+declare(strict_types=1);
 
 namespace Checkout\Placeholder\Block\Onepage;
 
@@ -9,31 +14,36 @@ use Magento\Framework\Stdlib\ArrayManager;
 class PlaceholderProcessor implements LayoutProcessorInterface
 {
     public const COMPONENT_SEARCH_KEY = 'searchKey';
-    public const COMPONENT_FIELDS_PATH = 'fieldsPath';
-    protected array $processList;
+    public const COMPONENT_SEARCH_PATH = 'path';
+    protected array $fieldLists;
+    protected array $fields;
     protected ArrayManager $arrayManager;
     protected PlaceholderHelper $placeholderHelper;
 
     public function __construct(
-        array $processList,
+        array $fieldLists,
+        array $fields,
         ArrayManager $arrayManager,
         PlaceholderHelper $placeholderHelper
     ) {
-        $this->processList = $this->validateProcessList($processList);
+        $this->fieldLists = $this->filterInvalidSearchItems($fieldLists);
+        $this->fields = $this->filterInvalidSearchItems($fields);
         $this->arrayManager = $arrayManager;
         $this->placeholderHelper = $placeholderHelper;
     }
-
+    
     /**
-     * Remove all addresses without COMPONENT_SEARCH_KEY.
+     * Remove not qualified search items.
      *
-     * @param  array $processList
+     * @param  array $searchItems
      * @return array
      */
-    public function validateProcessList(array $processList): array
+    public function filterInvalidSearchItems(array $searchItems): array
     {
-        return array_filter($processList, static function ($address) {
-            return array_key_exists(self::COMPONENT_SEARCH_KEY, $address);
+        return array_filter($searchItems, static function ($item) {
+            $keyAvailable = $item[self::COMPONENT_SEARCH_KEY] ?? false;
+            $pathAvailable = $item[self::COMPONENT_SEARCH_PATH] ?? false;
+            return $keyAvailable && $pathAvailable;
         });
     }
 
@@ -43,22 +53,51 @@ class PlaceholderProcessor implements LayoutProcessorInterface
      */
     public function process($jsLayout)
     {
-        foreach ($this->processList as $searchSettings) {
-            $componentPaths = $this->arrayManager->findPaths($searchSettings[self::COMPONENT_SEARCH_KEY], $jsLayout);
-            foreach ($componentPaths as $componentPath) {
-                $fullComponentPath = $componentPath . $searchSettings[self::COMPONENT_FIELDS_PATH];
-                $fields = $this->arrayManager->get($fullComponentPath, $jsLayout);
-                if ($fields) {
-                    $this->processFields($fullComponentPath, $fields, $jsLayout);
-                }
-            }
-        }
+        $this->processFieldLists($jsLayout);
+        $this->processFields($jsLayout);
         return $jsLayout;
     }
 
-    public function processFields(string $fullComponentPath, array $fields, array &$jsLayout): void
+    /**
+     * Get nodes by search criteria list.
+     */
+    protected function searchNodes(array $searchCriteria, array $jsLayout): \Generator
     {
-        $newFields = $this->placeholderHelper->convertFieldLabelsIntoPlaceholders($fields);
-        $jsLayout = $this->arrayManager->merge($fullComponentPath, $jsLayout, $newFields);
+        foreach($searchCriteria as $searchCriterion) {
+            $componentPaths = $this->arrayManager->findPaths($searchCriterion[self::COMPONENT_SEARCH_KEY], $jsLayout);
+            foreach ($componentPaths as $componentPath) {
+                $path = $componentPath . $searchCriterion[self::COMPONENT_SEARCH_PATH];
+                $node = $this->arrayManager->get($path, $jsLayout);
+                if($node) {
+                    yield $path => $node;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Search and convert all field lists.
+     * 
+     * @param $jsLayout
+     */
+    public function processFieldLists(&$jsLayout): void 
+    {
+        foreach($this->searchNodes($this->fieldLists, $jsLayout) as $path => $node) {
+            $this->placeholderHelper->convertFieldList($node);
+            $jsLayout = $this->arrayManager->merge($path, $jsLayout, $node);
+        };
+    }
+
+    /**
+     * Search and convert all specific fields.
+     * 
+     * @param $jsLayout
+     */
+    public function processFields(&$jsLayout): void
+    {
+        foreach($this->searchNodes($this->fields, $jsLayout) as $path => $node) {
+            $this->placeholderHelper->addPlaceholder($node);
+            $jsLayout = $this->arrayManager->merge($path, $jsLayout, $node);
+        }
     }
 }
--- README.md
+++ README.md
@@ -4,17 +4,12 @@ This module is designed to visually change all field pairs (label + field) so th
 
 The following field pairs will be changed:
 
-- All field pairs of the billing address
-- All field pairs of the shipping address
-- The field pair of the email address
-- The field pair of the comment field
+- All field pairs of the billing address (default luma, amasty checkout)
+- All field pairs of the shipping address (default luma, amasty checkout)
+- The field pair of the email address (default luma, amasty checkout, amazon pay)
+- The field pair of the comment field (amasty checkout)
 
 The labels remain untouched and are hidden via CSS.
-
-#### Compatibility
-
-* amzn/amazon-pay-magento-2-module:"5.7.1" (knockout email template override)
-* amasty/module-single-step-checkout:"3.1.2" (knockout email template override)
 
 #### Installation
 ```bash
@@ -22,13 +17,17 @@ composer require mediarox/module-checkout-placeholder
 bin/magento setup:upgrade
 ```
 
+#### Compatible with
+
+* amzn/amazon-pay-magento-2-module, tested: 5.7.1, 5.9.1
+* amasty/module-single-step-checkout, tested: 3.1.2, 3.1.3
+
+#### Wiki
+
+[How do I add a custom checkout field ?](https://github.com/mediarox/module-checkout-placeholder/wiki/How-do-I-add-a-custom-checkout-field-%3F)
+
+
 #### Visual - before & after
 
 ![without_extension](https://user-images.githubusercontent.com/32567473/134947126-6cfa3b11-a92b-45a0-90cf-63e7ba042722.png)
 ![with_extension](https://user-images.githubusercontent.com/32567473/134947141-b6c30ddb-ea68-43cb-a7c9-95df42488056.png)
-
-#### ToDo's
-* handling different count of street lines
-    * 1 = parent field label
-    * \> 1 = current field label ?
-* required mark string as config
